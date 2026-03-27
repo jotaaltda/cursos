5abd4a96fcf225a987ce59e2a1a591b36f8bcb82
Joao Paulo <jotape.hin@gmail.com>
2026-03-27 17:47:58 -0300

    Adiciona atividade 2

---
+++ Python/Atividades/atividade2.py
@@ -0,0 +1,16 @@
+# Atividade 2 - Calculadora de IMC!
+
+# Variáveis =================================================================================================
+
+nome = 'Luiz Otavio'
+altura = 1.80
+peso = 95
+imc = ...
+
+# ===========================================================================================================
+
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
