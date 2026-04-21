cc5ed3693898ec5bbd7d110522b592c7e3187372
Joao Paulo <jotape.hin@gmail.com>
2026-04-21 10:13:34 -0300

    Cria menu inicial da calculadora

---
+++ Python/Atividades/calculadora_while.py
@@ -0,0 +1,14 @@
+while True:
+    print('Digite a opção desejada!')
+    print('1. Calcular!')
+    print('2. Sair!')
+
+    opcao = int(input(''))
+
+    if opcao == 1:
+        continue
+            
+    if opcao == 2:
+        break
+    
+print('Volte sempre que desejar calcular algo novo!')
