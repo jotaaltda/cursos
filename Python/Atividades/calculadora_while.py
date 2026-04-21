5ac6857a5aabe67e57aaf21fc3e4ddbdf662f6cf
Joao Paulo <jotape.hin@gmail.com>
2026-04-21 10:22:09 -0300

    Cria a lógica das operações

--- Python/Atividades/calculadora_while.py
+++ Python/Atividades/calculadora_while.py
@@ -6,9 +6,24 @@ while True:
     opcao = int(input(''))
 
     if opcao == 1:
-        continue
+        while True:
+            numero_1 = float(input('Digite o primeiro número: '))
+            operador = input('Digite a operação desejada: ')
+            numero_2 = float(input('Digite o segundo número: '))
+
+            if operador == "+":
+                print(numero_1 + numero_2)
+                print('[R]eset / [V]oltar para o menu: ')
+            elif operador == "-":
+                print(numero_1 - numero_2)
+                print('[R]eset / [V]oltar para o menu: ')
+            elif operador == "*":
+                print(numero_1 * numero_2)
+                print('[R]eset / [V]oltar para o menu: ')
+            elif operador == "/":
+                print(numero_1 / numero_2)
+                print('[R]eset / [V]oltar para o menu: ')
             
     if opcao == 2:
+        print('Volte sempre que desejar calcular algo novo!')
         break
-    
-print('Volte sempre que desejar calcular algo novo!')
